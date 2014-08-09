0c59610f5c226391f529aa412ab268abe8a24c12
Nauktis <balzard@gmail.com>
2014-08-09 17:14:56 +0200

    Renamed the default example mod

--- src/main/java/com/nauktis/solarflux/SolarFluxMod.java
+++ src/main/java/com/nauktis/solarflux/SolarFluxMod.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package com.nauktis.solarflux;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = SolarFluxMod.MODID, version = SolarFluxMod.VERSION)
+public class SolarFluxMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
