7684e63446c9b7c06a5eb02cdb9c8d873c61ad4f
Harold <balzard@gmail.com>
2015-05-20 00:16:22 +0200

    Re-introduced the possibility to use a texture name different than the name of the block in BaseModBlock.

--- src/main/java/com/nauktis/core/block/BaseModBlock.java
+++ src/main/java/com/nauktis/core/block/BaseModBlock.java
@@ -3,36 +3,41 @@ package com.nauktis.core.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class BaseModBlock extends Block {
-	private final String mModId;
+    private final String mModId;
 
-	public BaseModBlock(String pModId) {
-		super(Material.rock);
-		mModId = Preconditions.checkNotNull(pModId).toLowerCase();
-	}
+    public BaseModBlock(String pModId) {
+        super(Material.rock);
+        mModId = checkNotNull(pModId).toLowerCase();
+    }
 
-	public BaseModBlock(String pModId, String pName) {
-		this(pModId);
-		setBlockName(Preconditions.checkNotNull(pName));
-	}
+    public BaseModBlock(String pModId, String pName) {
+        this(pModId);
+        setBlockName(checkNotNull(pName));
+        setBlockTextureName(unwrapUnlocalizedName(getUnlocalizedName()));
+    }
 
-	public String getModId() {
-		return mModId;
-	}
+    public String getBlockTextureName() {
+        return super.getTextureName();
+    }
 
-	@Override
-	public String getUnlocalizedName() {
-		return String.format("tile.%s:%s", getModId(), unwrapUnlocalizedName(super.getUnlocalizedName()));
-	}
+    @Override
+    public Block setBlockTextureName(String pTextureName) {
+        return super.setBlockTextureName(checkNotNull(pTextureName));
+    }
 
-	@Override
-	protected String getTextureName() {
-		return unwrapUnlocalizedName(getUnlocalizedName());
-	}
+    public String getModId() {
+        return mModId;
+    }
 
-	protected String unwrapUnlocalizedName(String pUnlocalizedName) {
-		return pUnlocalizedName.substring(pUnlocalizedName.indexOf(".") + 1);
-	}
+    @Override
+    public String getUnlocalizedName() {
+        return String.format("tile.%s:%s", getModId(), unwrapUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    protected String unwrapUnlocalizedName(String pUnlocalizedName) {
+        return pUnlocalizedName.substring(pUnlocalizedName.indexOf(".") + 1);
+    }
 }
