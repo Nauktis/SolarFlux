fe01488586c20f851b4c11604e0f5c60a1dcc601
Harold <balzard@gmail.com>
2015-08-04 16:40:23 +0200

    Fix

--- src/main/java/com/nauktis/solarflux/config/ModConfiguration.java
+++ src/main/java/com/nauktis/solarflux/config/ModConfiguration.java
@@ -69,8 +69,8 @@ public class ModConfiguration {
         mSolarPanelHeight = mConfiguration.getFloat(
                 "SolarPanelHeight",
                 Configuration.CATEGORY_GENERAL,
+                0.375F,
                 0.01F,
-                0.1F,
                 1,
                 "The height of the Solar Panel blocks.");
         mAutoBalanceEnergy = mConfiguration.getBoolean(
